b9f451f37eae618ce9aac224e894945fe56184fe
tinozxc <tino234567@hotmail.com>
2024-11-24 23:31:40 -0500

    Otra forma de eliminar le ultimo caracter

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function limpiarPantalla(){
   }
 
   function deshacer(){ 
-    const valorPantalla =  document.getElementById('pantalla').value ;
-    document.getElementById('pantalla').value = valorPantalla.slice(0,-1)
+     const valorPantalla = document.getElementById('pantalla').value;
+      const resultado = valorPantalla.substring(0,valorPantalla.length -1);
+      document.getElementById('pantalla').value = resultado;
   }
